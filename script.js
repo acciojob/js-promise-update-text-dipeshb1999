150c7dd3d90f4f3f2bcd633b6a87eb38a9c80101
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-31 17:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Get the element with ID "output"
+const outputElement = document.getElementById('output');
+
+// Function that returns a promise
+function delayedGreeting() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+// Call the function and update the text of the output element after the promise resolves
+delayedGreeting()
+  .then(result => {
+    outputElement.innerText = result;
+  });
